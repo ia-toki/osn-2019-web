df9a6c2fbd085c9fb15538ee8b8cdec2f1aff0e8
Ashar Fuadi <fushar@gmail.com>
2019-06-29 20:10:03 +0700

    Add scoreboard template

---
+++ assets/js/config.js
@@ -0,0 +1,382 @@
+var config = {
+  serverUrl:
+    "http://scoreboard.toki.id/serve?contestJid=xxx",
+  refreshIntervalInMs: 5000,
+  contestants: {
+    JIDUSERA4RKQgQZswM9EArLzzqK: {
+      name: "Yusuf Ilham",
+      school: "MAN 2 Pekanbaru",
+      province: "Riau"
+    },
+    JIDUSERH4dJwgJBzwW0SwrEue52: {
+      name: "M. Aldy Ainur Ahmad",
+      school: "SMAN 1 Polewali",
+      province: "Sulawesi Barat"
+    },
+    JIDUSERkkY5j9Dt5TcrD6UeKn9Z: {
+      name: "Muhammad Arif Nuriman",
+      school: "SMAN 5 Bekasi",
+      province: "Jawa Barat"
+    },
+    JIDUSERf9XKlNEQARLRESU5KmHu: {
+      name: "Akmal Hamdalah Banawata",
+      school: "MAN Insan Cendekia Paser",
+      province: "Kalimantan Timur"
+    },
+    JIDUSERpK7xRXMygpICO0FgexYj: {
+      name: "Banabil Fawazaim Muhammad",
+      school: "SMA Cahaya Rancamaya IBS",
+      province: "Jawa Barat"
+    },
+    JIDUSERtNnTptkq5WYx2ObZk3cs: {
+      name: "Edbert Geraldy Cangdinata",
+      school: "SMA Sutomo 1 Medan",
+      province: "Sumatera Utara"
+    },
+    JIDUSERMmSxozaoyZN3QE4THEIe: {
+      name: "Gregorius Bhisma",
+      school: "SMAN 2 Kota Tangerang Selatan",
+      province: "Banten"
+    },
+    JIDUSERh0wgmSsBJphA3hpk3ZYf: {
+      name: "Bonaventura Galang Kristabel Angipanglipur Hatmasasmita",
+      school: "SMA Santo Yosef Pangkalpinang",
+      province: "Kepulauan Bangka Belitung"
+    },
+    JIDUSERpwDuQhgsFfob3hzkdGJH: {
+      name: "Ardi Caesar Kurniawan",
+      school: "SMA Sains Averos",
+      province: "Papua Barat"
+    },
+    JIDUSERbYpf7m9nFPazrnKf7EWS: {
+      name: "Anak Agung Ngurah Andhika Satrya Nugraha",
+      school: "SMAN 1 Denpasar",
+      province: "Bali"
+    },
+    JIDUSER5VZmEGyeA8WmxWhKLRsw: {
+      name: "Danindra Ario Wiryawan",
+      school: "SMAN 1 Yogyakarta",
+      province: "DI Yogyakarta"
+    },
+    JIDUSERRX2dSKt2ZC8b5oviU3N6: {
+      name: "Dwima Agus Nurcahya Putra",
+      school: "SMAN 1 Tanjung",
+      province: "Nusa Tenggara Barat"
+    },
+    JIDUSERKIkpIQTUylPPQ0xmmXs4: {
+      name: "Ekasari Amalia",
+      school: "SMA Al Azhar Mandiri Palu",
+      province: "Sulawesi Tengah"
+    },
+    JIDUSER3Nq6F85OZzISf4OFuhjQ: {
+      name: "Fadhil Aldito Putra",
+      school: "SMAN 8 Pekanbaru",
+      province: "Riau"
+    },
+    JIDUSERT1knQHSGOtP1XBgiGcqG: {
+      name: "Faegan Lemuel Davlyson",
+      school: "SMAK Penabur Gading Serpong",
+      province: "Banten"
+    },
+    JIDUSERoGRSCC6aLd7YYVHGfwVE: {
+      name: "Muhammad Fauzul Akbar",
+      school: "SMA Unggulan Ct Foundation",
+      province: "Sumatera Utara"
+    },
+    JIDUSERzA9jdPff5J8Km26zBM3b: {
+      name: "Fatih Mahawisesa",
+      school: "SMAN 1 Sungai Penuh",
+      province: "Jambi"
+    },
+    JIDUSERSwwMLNdDrAGdl85vf0EL: {
+      name: "William Owen Gozali",
+      school: "SMA British School Jakarta",
+      province: "Banten"
+    },
+    JIDUSERbzytnBDyg9XsxZKRmW6Z: {
+      name: "Gwyneth Guo Bai Ling",
+      school: "SMA Kristen Intan Permata Hati",
+      province: "Jawa Timur"
+    },
+    JIDUSER2gg9fH73aaQnrUruoUrH: {
+      name: "Muhammad Habibi Husni",
+      school: "SMAN 2 Payakumbuh",
+      province: "Sumatera Barat"
+    },
+    JIDUSERRpDNb3QhM7yGwEprtmHv: {
+      name: "Hafizha Ulinnuha Ahmad",
+      school: "SMA Yayasan Pupuk Kaltim",
+      province: "Kalimantan Timur"
+    },
+    JIDUSERrwlzDV8ggcqGgmOAv0bn: {
+      name: "Hafizh Abiyaniqbal Harahap",
+      school: "SMAN 1 Batam",
+      province: "Kepulauan Riau"
+    },
+    JIDUSERKiRDEllRg7skebP6UReR: {
+      name: "Muhammad Halif",
+      school: "SMAN 1 Depok",
+      province: "Jawa Barat"
+    },
+    JIDUSERVQ7RjTL8MZoz0fNHoqKG: {
+      name: "Muhammad Hamlul Khair",
+      school: "SMAN Modal Bangsa Aceh",
+      province: "Aceh"
+    },
+    JIDUSERYBFZMisdnL1xaRTdUWGX: {
+      name: "Muhammad Iqbal Asrif",
+      school: "SMAN 3 Malang",
+      province: "Jawa Timur"
+    },
+    JIDUSERe5FSD5vCQYNK0m323Orm: {
+      name: "Johann Christian Kandani",
+      school: "SMA Katolik Rajawali Makassar",
+      province: "Sulawesi Selatan"
+    },
+    JIDUSERlmInid8Lmj3Fyv6rdWeq: {
+      name: "Juan Carlo Vieri",
+      school: "SMP Kristen Intan Permata Hati",
+      province: "Jawa Timur"
+    },
+    JIDUSERmCge0gWZeak8xn5GTIHu: {
+      name: "Justin Andhika Chandra",
+      school: "SMAN 1 Singaraja",
+      province: "Bali"
+    },
+    JIDUSER0KSXskAyGUvRiG2cBieQ: {
+      name: "Kausar Meutuwah",
+      school: "SMA YPVDP Bontang",
+      province: "Kalimantan Timur"
+    },
+    JIDUSER1LMJQscmJECx2oJ3O1B3: {
+      name: "Kenneth Nicholas Mamahit",
+      school: "SMA Kristen Eben Haezar Manado",
+      province: "Sulawesi Utara"
+    },
+    JIDUSERdcrWa9P4Jy1iCJg946ph: {
+      name: "Muhammad Kenshin Himura Mahmuddin",
+      school: "SMAN 1 Kota Bogor",
+      province: "Jawa Barat"
+    },
+    JIDUSERHEcABbRrjoEML1nPNxpl: {
+      name: "Reza Ali Nirwansyah",
+      school: "SMAN 1 Pangkalan Bun",
+      province: "Kalimantan Tengah"
+    },
+    JIDUSERD6TosALOoUuJWdk7dPaX: {
+      name: "Mustofa Kamal",
+      school: "SMAN 1 Tanjungpandan",
+      province: "Kepulauan Bangka Belitung"
+    },
+    JIDUSERNwsWbTlqewq1dFXdaYA9: {
+      name: "Kevin Jonathan Kusnomo",
+      school: "SMAK 5 BPK PENABUR Jakarta",
+      province: "DKI Jakarta"
+    },
+    JIDUSERNZbvdoUPFFFfUnqBC9Ww: {
+      name: "Markus Leonard Wijaya",
+      school: "SMAN 3 Semarang",
+      province: "Jawa Tengah"
+    },
+    JIDUSERPGOZLsP3W8r61QKl0LjI: {
+      name: "Maximilliano Utomo Quok",
+      school: "SMP Xin Zhong School",
+      province: "Jawa Timur"
+    },
+    JIDUSERaS9Ltv87IjNyMaMFzmqE: {
+      name: "Amanda Nurul Izzah",
+      school: "SMA Teuku Nyak Arif Fatih Bilingual School",
+      province: "Aceh"
+    },
+    JIDUSERStIviZo5UbyaIDxUS4OC: {
+      name: "Muhammad Malik Aditya Kurniawan",
+      school: "SMA Taruna Nusantara",
+      province: "Jawa Tengah"
+    },
+    JIDUSERxdvktNGcd3JQVyzYKsYp: {
+      name: "Nabil Ibadurrahman Ervatra",
+      school: "SMPN 115 Jakarta",
+      province: "DKI Jakarta"
+    },
+    JIDUSERqv2A8WNBCWSIOtS7O9bV: {
+      name: "Cangnai Fang",
+      school: "SMAN 7 Banjarmain",
+      province: "Kalimantan Selatan"
+    },
+    JIDUSERevWq2mxg7AgyXIspoEW7: {
+      name: "Noverdi Anugrah Ramadhan",
+      school: "SMAN 1 Satui",
+      province: "Kalimantan Selatan"
+    },
+    JIDUSERKGeHOLuN4l9PgdeEGCRp: {
+      name: "Ahmad.mudhiya Ananta Rahman",
+      school: "MAN Insan Cendekia",
+      province: "Kalimantan Selatan"
+    },
+    JIDUSERty3ojBuAA0EnmbIqtVia: {
+      name: "Theophilus Joseph Wahyudi",
+      school: "SMA Santa Laurensia",
+      province: "Banten"
+    },
+    JIDUSERJmayvusq2A5P2jfWTf3X: {
+      name: "Putu Andhika Pratama",
+      school: "SMAN 3 Kupang",
+      province: "Nusa Tenggara Timur"
+    },
+    JIDUSERtrocd6vOwPytp6AUml0z: {
+      name: "Pikatan Arya Bramajati",
+      school: "SMA Semesta BBS Semarang",
+      province: "Jawa Tengah"
+    },
+    JIDUSERFwOLcKyUnmiaxZQI4xX0: {
+      name: "Rahadian Panji Ramadhan",
+      school: "SMAN 1 Purwokerto",
+      province: "Jawa Tengah"
+    },
+    JIDUSER6jsty9edtiHptmF0Oxk5: {
+      name: "Rama Aryasuta Pangestu",
+      school: "SMA Kanisius Jakarta",
+      province: "DKI Jakarta"
+    },
+    JIDUSERggrJNTMJZBbUfNP1mMIE: {
+      name: "Dwi Rangga Rhaditya Siwi Widodo",
+      school: "SMAN 1 Pontianak",
+      province: "Kalimantan Barat"
+    },
+    JIDUSERK79rnHCPelYbwjpty56W: {
+      name: "Muhammad Zhafran Ammar",
+      school: "SMAN 2 Tarakan",
+      province: "Kalimantan Utara"
+    },
+    JIDUSER7zp3Oyzz5AXvvgo3BPL4: {
+      name: "Jubril Biruni Pamboke",
+      school: "SMAN 4 Kendari",
+      province: "Sulawesi Tenggara"
+    },
+    JIDUSERoHRWnEkqpaSmX1XnBmDH: {
+      name: "Rheza Rizqullah Ecaldy",
+      school: "SMA Kusuma Bangsa Palembang",
+      province: "Sumatera Selatan"
+    },
+    JIDUSER6GLLPWBXNCANpKXVhEa6: {
+      name: "Richie Christiansen Senlia",
+      school: "SMA Xaverius 1 Jambi",
+      province: "Jambi"
+    },
+    JIDUSERZpYO43uDIrcM7LiphBNy: {
+      name: "Haniif Ahmad Jauhari",
+      school: "SMAN 1 Sooko",
+      province: "Jawa Timur"
+    },
+    JIDUSERUaUwJ0zXcAUteyjSyaz5: {
+      name: "Tan Caken",
+      school: "SMAK Yos Sudarso Batam",
+      province: "Kepulauan Riau"
+    },
+    JIDUSERss3bIUJ7qQLR6cCbbOI6: {
+      name: "Andrew Christian Jubintoro",
+      school: "SMA Manado Independent School",
+      province: "Sulawesi Utara"
+    },
+    JIDUSERSINHjEvlBW4NE2UzgjPL: {
+      name: "Fadli Dwi Ramadhan",
+      school: "SMAIT IQRA' Kota Bengkulu",
+      province: "Bengkulu"
+    },
+    JIDUSERDtqo5xB5hvOlgdmd4Cv2: {
+      name: "Afif Syaifullah Fattah",
+      school: "SMA Al Kautsar Bandar Lampung",
+      province: "Lampung"
+    },
+    JIDUSERL9QljWkr7ar485TUqWqa: {
+      name: "Nabil Muhammad Azzam",
+      school: "MAN Insan Cendekia Gorontalo",
+      province: "Gorontalo"
+    },
+    JIDUSERK2gMTIbYcfAxfUpmtRlX: {
+      name: "Yohandi",
+      school: "SMAK 1 BPK PENABUR Jakarta",
+      province: "DKI Jakarta"
+    },
+    JIDUSERui80QlReEYBDrb2tmQ4y: {
+      name: "Steven Adrian Gracia",
+      school: "SMA Kristen Petra 1 Surabaya",
+      province: "Jawa Timur"
+    },
+    JIDUSERjcmKGrNUP0Ftckqb4ymf: {
+      name: "Steven Novaryo",
+      school: "SMA Katolik Santu Petrus Pontianak",
+      province: "Kalimantan Barat"
+    },
+    JIDUSERldopgbyWyQk8qjgUJfBu: {
+      name: "Sulthan Afif Althaf",
+      school: "SMAN Modal Bangsa Aceh",
+      province: "Aceh"
+    },
+    JIDUSERmBiuo9uniwEKBNtUHCDM: {
+      name: "Amelia Tanwijaya",
+      school: "SMA Methodist 2 Medan",
+      province: "Sumatera Utara"
+    },
+    JIDUSER0IQK01iL1N24j62GVkVI: {
+      name: "Tiara Firgishanda Ipaenin",
+      school: "SMAN 2 Maohi",
+      province: "Maluku"
+    },
+    JIDUSERMCTvw7oQQvBBUhSW1Qry: {
+      name: "William Alexander Tanex",
+      school: "SMA Kristen Eben Haezar Manado",
+      province: "Sulawesi Utara"
+    },
+    JIDUSERPnMdPwdfz6cJfVybB9si: {
+      name: "Umar Wira Hadi Aspar Amini",
+      school: "SMAN 2 Jayapura",
+      province: "Papua"
+    },
+    JIDUSERbXsuo1joeijhAMG5GXkt: {
+      name: "Vinsen Nawir",
+      school: "SMA Katolik Rajawali Makassar",
+      province: "Sulawesi Selatan"
+    },
+    JIDUSERObeKbn0tXNzHvpIw7qCw: {
+      name: "Vio Albert Ferdinand",
+      school: "SMA Ignatius Global School Palembang",
+      province: "Sumatera Selatan"
+    },
+    JIDUSERSDKzeaRz0hlE61W4pRsJ: {
+      name: "Wesley Susanto",
+      school: "SMA Ignatius Global School Palembang",
+      province: "Sumatera Selatan"
+    },
+    JIDUSER8LASq5EsmUKUaDeYNzfY: {
+      name: "Yohanes Pandu Kilang Ewang Ewang",
+      school: "SMAN 1 Metro",
+      province: "Lampung"
+    },
+    JIDUSERNQhpfG4N3R8jAINBeRMm: {
+      name: "Yumna Aqila Rasyidah",
+      school: "SMAN 2 Purwokerto",
+      province: "Jawa Tengah"
+    },
+    JIDUSERa5wb4JFLvvpsSRG7jjU3: {
+      name: "Mirza Safaraz",
+      school: "SMAN 1 Yogyakarta",
+      province: "DI Yogyakarta"
+    },
+    JIDUSERK6MYnqnGisOHQYgRknQK: {
+      name: "Zikru Akbar Isnain",
+      school: "SMAN 8 Kota Ternate",
+      province: "Maluku Utara"
+    },
+    JIDUSERXFAG0Yujd4gpltprduKm: {
+      name: "Muhammad Zikri Ramadhan",
+      school: "SMAN 1 Padang",
+      province: "Sumatera Barat"
+    },
+    JIDUSERmCbuR9FY0eZ5WZZQASEH: {
+      name: "Ahmad Ramadhan",
+      school: "MAN Insan Cendekia Gorontalo",
+      province: "Gorontalo"
+    }
+  }
+};
